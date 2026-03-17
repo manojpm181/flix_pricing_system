ba8b8f110e99a7e834b2f8edcd91f97204d82011
Manoj P M <159712848+manojpm181@users.noreply.github.com>
2026-03-17 20:10:03 +0530

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 # ---------------- PAGE CONFIG ----------------
 st.set_page_config(page_title="Bus Pricing Dashboard", layout="wide")
